30a8d4a554491afda6b03ea156a274e70839d488
Rejoy01 <rejoywilson65@gmail.com>
2024-01-13 21:03:32 +0530

    home

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Home.css"
+import BreifCase from "../img/Briefcase.png"
+const Home = () => {
+  return (
+    <div className='home'>
+        <div className="left-side">
+            <div>
+                <img src={BreifCase} alt="" />
+            </div>
+        </div>
+        <div className='right-side'>
+            Dashboard aksbjgdhabs
+        </div>
+    </div>
+  )
+}
+
+export default Home
